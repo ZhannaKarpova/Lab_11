9ceeb5396fe2da3aaac95ebd6f5171c211991f06
ZhannaKarpova <92864174+ZhannaKarpova@users.noreply.github.com>
2021-11-18 20:57:22 +0700

    Добавьте файлы проекта.

---
+++ Lab_11/MyClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lab_11
+{
+    partial class Program
+    {
+        class MyClass
+        {
+
+            private double first;
+            private double second;
+            private double funk;
+            private double x;
+
+            public MyClass(double A, double B, double Y)
+            {
+                first = A;
+                second = B;
+                funk = Y;
+            }
+            public void Read(double a, double b, double y)
+            {
+                first = a;
+                second = b;
+                funk = y;
+                root();
+            }
+            public void root()
+            {
+                if (first != 0) x = (funk - second) / first;
+                Display();
+            }
+            public void Display()
+            {
+                Console.WriteLine("Значение х : {0}", x);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Lab_11/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11
+{
+    class MyClass
+    {
+        static void ShowEmployee()
+        {
+            new Equalissation();
+        }
+        struct Equalissation { }
+
+        private double first;
+        private double second;
+        private double funk;
+        private double x;
+
+        public MyClass(double k, double b)
+        {
+            first = k;
+            second = b;
+            funk = 0;
+        }
+        public void Read(double k, double b)
+        {
+            first = k;
+            second = b;
+            funk = 0;
+            root();
+        }
+        public void root()
+        {
+            if (first != 0) x = (funk - second) / first;
+            Display();
+        }
+        public void Display()
+        {
+            Console.WriteLine("Значение х : {0}", x);
+            Console.ReadKey();
+        }
+    }
+    class myProgram
+    {
+        static void Main(string[] args)
+        {
+
+            MyClass qwr = new MyClass(0, 0);
+            Console.Write("Введите значение k: ");
+            double h = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите значение b: ");
+            double j = 0;
+            double s = Convert.ToDouble(Console.ReadLine());
+            qwr.Read(h,s);
+
+        }
+    } }
